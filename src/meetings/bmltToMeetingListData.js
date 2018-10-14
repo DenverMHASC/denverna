b8f3ecf32d087190262143aaee265569d2c0ae6d
Webservant <webservant.mhasc@gmail.com>
2018-10-14 11:49:06 -0600

    remove extra code

--- src/meetings/bmltToMeetingListData.js
+++ src/meetings/bmltToMeetingListData.js
@@ -2,46 +2,6 @@ import moment from 'moment'
 import momentDurationFormatSetup from "moment-duration-format"
 momentDurationFormatSetup(moment);
 
-const foo = {
-    bus_lines: "",
-    comments: "",
-    contact_email_1: "",
-    contact_email_2: "",
-    contact_name_1: "",
-    contact_name_2: "",
-    contact_phone_1: "",
-    contact_phone_2: "",
-    distance_in_km: "",
-    distance_in_miles: "",
-    duration_time: "01:00:00",
-    email_contact: "",
-    format_shared_id_list: "17,8",
-    formats: "O,D",
-    id_bigint: "238",
-    lang_enum: "en",
-    latitude: "39.7409423",
-    location_city_subsection: "",
-    location_info: "",
-    location_municipality: "Lakewood",
-    location_nation: "USA",
-    location_neighborhood: "",
-    location_postal_code_1: "80215",
-    location_province: "CO",
-    location_street: "1510 Glen Ayr Dr. #12",
-    location_sub_province: "Jefferson",
-    location_text: "",
-    longitude: "-105.0956871",
-    meeting_name: "Earlybirds",
-    published: "1",
-    root_server_uri: "https://nacolorado.org/meetingList/main_server",
-    service_body_bigint: "2",
-    shared_group_id_bigint: "",
-    start_time: "07:00:00",
-    train_lines: "",
-    weekday_tinyint: "7",
-    worldid_mixed: "G00019897",
-}
-
 const buildNotesString = (text, info) => {
     if (text && info) {
         return `${text}, ${info}`
