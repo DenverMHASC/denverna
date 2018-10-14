2ced1c858250973a9828c4a1b292bf5ad1495649
denvernaaf <arlofeirman+1@gmail.com>
2018-10-13 20:14:56 -0600

    Basic app component

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = (props) => {
+  return (
+    <div>The app</div>
+  )
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import { renderMeetingList } from './meetings/meetings.js';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
-
-renderMeetingList()
+ReactDOM.render(<App />, document.getElementById("root"))
