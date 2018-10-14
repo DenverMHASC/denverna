94348e5877fa89e4af7f311f37b1235f40c767f6
denvernaaf <arlofeirman+1@gmail.com>
2018-10-13 21:15:17 -0600

    Bring in react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Header from './components/Header'
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { Switch, Route, BrowserRouter } from 'react-router-dom'
+import Home from './views/Home'
 
 
 const App = (props) => {
@@ -8,6 +10,13 @@ const App = (props) => {
     <React.Fragment>
       <CssBaseline />
       <Header />
+      <BrowserRouter>
+        <Switch>
+          <Route path='/' component={Home} />
+        </Switch>
+
+      </BrowserRouter>
+
     </React.Fragment>
   )
 }
