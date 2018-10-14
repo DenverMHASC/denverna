02b1d7616aec7bc17b628e1875d8fec9b03782de
Webservant <webservant.mhasc@gmail.com>
2018-10-14 11:46:47 -0600

    use BMLT as source of truth

--- src/meetings/bmltToMeetingListData.js
+++ src/meetings/bmltToMeetingListData.js
@@ -1,4 +1,6 @@
 import moment from 'moment'
+import momentDurationFormatSetup from "moment-duration-format"
+momentDurationFormatSetup(moment);
 
 const foo = {
     bus_lines: "",
@@ -13,56 +15,83 @@ const foo = {
     distance_in_miles: "",
     duration_time: "01:00:00",
     email_contact: "",
-    formats: "O,D,WC",
-    id_bigint: "164",
+    format_shared_id_list: "17,8",
+    formats: "O,D",
+    id_bigint: "238",
     lang_enum: "en",
-    latitude: "39.6241751",
+    latitude: "39.7409423",
     location_city_subsection: "",
     location_info: "",
-    location_municipality: "Englewood",
+    location_municipality: "Lakewood",
     location_nation: "USA",
     location_neighborhood: "",
-    location_postal_code_1: "80111",
+    location_postal_code_1: "80215",
     location_province: "CO",
-    location_street: "5400 S. Yosemite",
-    location_sub_province: "Arapahoe",
+    location_street: "1510 Glen Ayr Dr. #12",
+    location_sub_province: "Jefferson",
     location_text: "",
-    longitude: "-104.8853408",
-    meeting_name: "Bring Your Own Lunch",
+    longitude: "-105.0956871",
+    meeting_name: "Earlybirds",
     published: "1",
+    root_server_uri: "https://nacolorado.org/meetingList/main_server",
     service_body_bigint: "2",
     shared_group_id_bigint: "",
-    start_time: "12:00:00",
+    start_time: "07:00:00",
     train_lines: "",
-    weekday_tinyint: "3",
-    worldid_mixed: "G00106832",
+    weekday_tinyint: "7",
+    worldid_mixed: "G00019897",
 }
 
-export const meetingConv = (bmltMeeting) => ({
-    time: moment(bmltMeeting.start_time, 'HH:mm:ss').format('h:mm a'),
-    name: bmltMeeting.meeting_name,
-    format: bmltMeeting.format.split(','),
-    address: {
-        street: bmltMeeting.location_street,
-        unit: '',
-        city: bmltMeeting.location_municipality,
-        zip: bmltMeeting.location_postal_code_1,
-        notes: bmltMeeting.location_text
+const buildNotesString = (text, info) => {
+    if (text && info) {
+        return `${text}, ${info}`
     }
-})
+    if (text && !info) {
+        return text
+    }
+    if (!text && info) {
+        return info
+    }
+    return ''
+}
+
+export const meetingConv = (bmltMeeting) => {
+    const duration = bmltMeeting.duration_time !== "01:00:00"
+        ? moment.duration(bmltMeeting.duration_time, 'HH:mm:ss').format('mm [min]')
+        : null
+
+    const format = duration
+        ? bmltMeeting.formats.split(',').concat(duration)
+        : bmltMeeting.formats.split(',')
+
+    return {
+        time: moment(bmltMeeting.start_time, 'HH:mm:ss').format('h:mm a'),
+        name: bmltMeeting.meeting_name,
+        format,
+        sortStartTime: bmltMeeting.start_time,
+        address: {
+            city: bmltMeeting.location_municipality,
+            notes: buildNotesString(bmltMeeting.location_text, bmltMeeting.location_info),
+            street: bmltMeeting.location_street,
+            unit: '',
+            zip: bmltMeeting.location_postal_code_1,
+        }
+    }
+}
 
 const numberDayToHumanReadable = {
     1: 'sunday',
-    2: 'tuesday',
-    3: 'wednesday',
-    4: 'thursday',
-    5: 'friday',
-    6: 'saturday',
+    2: 'monday',
+    3: 'tuesday',
+    4: 'wednesday',
+    5: 'thursday',
+    6: 'friday',
+    7: 'saturday',
 }
 
 export const bmltResponseToMeetingData = (body) => {
     return body.reduce((acc, bmltMeeting) => {
-        acc[numberDayToHumanReadable[bmltMeeting.weekday_tinyint]].concat(meetingConv(bmltMeeting))
+        acc[numberDayToHumanReadable[bmltMeeting.weekday_tinyint]].push(meetingConv(bmltMeeting))
         return acc
     }, { sunday: [], monday: [], tuesday: [], wednesday: [], thursday: [], friday: [], saturday: [] })
 }
