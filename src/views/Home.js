100578af8e42c3992b274a095211090ed4516a1b
denvernaaf <arlofeirman+1@gmail.com>
2018-10-13 21:05:59 -0600

    Create home component

---
+++ src/views/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Typography, Grid } from '@material-ui/core'
+
+const Home = (props) => {
+  return (
+    <Grid container>
+      <Typography>
+        Narcotics Anonymous is a nonprofit Fellowship or society of men and women for whom drugs had become a major problem. We are recovering addicts who meet regularly to help each other stay clean. This is a program of complete abstinence from all drugs. There is only one requirement for membership, the desire to stop using. We suggest that you keep an open mind and give yourself a break. Our program is a set of principles written so simply that we can follow them in our daily lives. The most important thing about them is that they work.
+        Basic Text, chapter two,
+        What is the Narcotics Anonymous Program, page 9
+      </Typography>
+    </Grid>
+  )
+}
+
+export default Home
