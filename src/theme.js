b8186802c544347be693fe92a85c058f6bb6ee99
denvernaaf <arlofeirman+1@gmail.com>
2018-10-26 22:57:37 -0400

    move theme into its own file and add colors

---
+++ src/theme.js
@@ -0,0 +1,19 @@
+import { createMuiTheme } from '@material-ui/core'
+
+export default createMuiTheme({
+  palette: {
+    primary: {
+      main: '#03a9f4',
+      light: '#67daff',
+      dark: '#007ac1',
+    },
+    secondary: {
+      main: '#607d8c',
+      light: '#8eacbc',
+      dark: '#34515f'
+    }
+  },
+  typography: {
+    useNextVariants: true,
+  },
+})
