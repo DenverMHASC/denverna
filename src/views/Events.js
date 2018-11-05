863e8c9de3afa45bdf96fb8a62b165b48e671ab9
Webservant <webservant.mhasc@gmail.com>
2018-11-05 15:18:03 -0700

    clean up calendar

--- src/views/Events.js
+++ src/views/Events.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Events = (props) => {
   return (
-    <iframe src="https://calendar.google.com/calendar/embed?src=e3u3bfsuo5uus3dfa12q4pk8mk%40group.calendar.google.com&ctz=America%2FDenver" width="800" height="600" frameBorder="0" scrolling="no"></iframe>
+    <iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showPrint=0&amp;showCalendars=0&amp;mode=AGENDA&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=e3u3bfsuo5uus3dfa12q4pk8mk%40group.calendar.google.com&amp;color=%23182C57&amp;ctz=America%2FDenver" width="800" height="600" frameBorder="0" scrolling="no"></iframe>
   )
 }
 
