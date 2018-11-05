826d452db88cb9ff21d4d679f9b5dd02de0fd66a
Webservant <webservant.mhasc@gmail.com>
2018-11-05 15:07:51 -0700

    adds calendar for events

--- src/views/Events.js
+++ src/views/Events.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Events = (props) => {
   return (
-    <p>Events Calendar</p>
+    <iframe src="https://calendar.google.com/calendar/embed?src=e3u3bfsuo5uus3dfa12q4pk8mk%40group.calendar.google.com&ctz=America%2FDenver" width="800" height="600" frameBorder="0" scrolling="no"></iframe>
   )
 }
 
