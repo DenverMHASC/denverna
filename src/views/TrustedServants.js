1080d55c7a69570a2b75811ad7866a0a51d297bf
Webservant MHASC <webservant.mhasc@gmail.com>
2019-01-13 16:37:46 -0700

    updates to TS page

--- src/views/TrustedServants.js
+++ src/views/TrustedServants.js
@@ -9,25 +9,24 @@ import {
 import FolderIcon from '@material-ui/icons/Folder';
 import FileCopyIcon from '@material-ui/icons/FileCopy';
 
-
 const TrustedServants = (props) => {
-  const style = {
-    width: '100%',
-    height: '600px'
-  }
   return (
     <div>
       <Typography variant="h6">Trust Servants Resources</Typography>
       <div>
         <List dense={true}>
-          <ListItem>
-            <ListItemIcon><FolderIcon /></ListItemIcon>
-            <ListItemText primary="Area Minutes Archive" />
-          </ListItem>
-          <ListItem>
-            <ListItemIcon><FileCopyIcon /></ListItemIcon>
-            <ListItemText primary="Literature Order Form" />
-          </ListItem>
+          <a target="_new" style={{ textDecoration: 'none' }} href="https://drive.google.com/drive/folders/19EQDeMd0vpdiZDd9Bai_OOOXsQTVTN6F?usp=sharing">
+            <ListItem>
+              <ListItemIcon><FolderIcon /></ListItemIcon>
+              <ListItemText primary="Area Minutes Archive" />
+            </ListItem>
+          </a>
+          <a target="_new" style={{ textDecoration: 'none' }} href="https://drive.google.com/drive/folders/1CbOPzwhE5LVG0-ZX1NlwvoEgwCoGkfmt">
+            <ListItem>
+              <ListItemIcon><FileCopyIcon /></ListItemIcon>
+              <ListItemText primary="Literature Order Forms" />
+            </ListItem>
+          </a>
         </List>
       </div>
     </div >
