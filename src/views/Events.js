bd147bba0afa60ff014d1d6a18adcd6d7e98087d
Webservant MHASC <webservant.mhasc@gmail.com>
2019-01-23 13:09:13 -0700

    use get-sheet-done to create links for trusted servants

--- src/views/Events.js
+++ src/views/Events.js
@@ -6,7 +6,9 @@ const Events = (props) => {
     height: '600px'
   }
   return (
-    <iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showPrint=0&amp;showCalendars=0&amp;mode=AGENDA&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=e3u3bfsuo5uus3dfa12q4pk8mk%40group.calendar.google.com&amp;color=%23182C57&amp;ctz=America%2FDenver" style={style} frameBorder="0" scrolling="no"></iframe>
+    <div>
+      <iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showPrint=0&amp;showCalendars=0&amp;mode=AGENDA&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=e3u3bfsuo5uus3dfa12q4pk8mk%40group.calendar.google.com&amp;color=%23182C57&amp;ctz=America%2FDenver" style={style} frameBorder="0" scrolling="no"></iframe>
+    </div>
   )
 }
 
--- src/views/TrustedServants.js
+++ src/views/TrustedServants.js
@@ -8,29 +8,59 @@ import {
 } from '@material-ui/core'
 import FolderIcon from '@material-ui/icons/Folder';
 import FileCopyIcon from '@material-ui/icons/FileCopy';
+import LinkIcon from '@material-ui/icons/LinkSharp';
+import GetSheetDone from 'get-sheet-done'
 
-const TrustedServants = (props) => {
-  return (
-    <div>
-      <Typography variant="h6">Trust Servants Resources</Typography>
+const DOCUMENT_ID = "1BLFmqqeuhRJpSagt2NWks8kTYfJ-sMCjYJtzyC8krh0"
+
+
+class TrustedServants extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      links: []
+    }
+  }
+
+  componentDidMount() {
+    GetSheetDone.labeledCols(DOCUMENT_ID).then(sheet => {
+      this.setState({ links: sheet.data })
+    })
+  }
+
+  render() {
+    return (
       <div>
-        <List dense={true}>
-          <a target="_new" style={{ textDecoration: 'none' }} href="https://drive.google.com/drive/folders/19EQDeMd0vpdiZDd9Bai_OOOXsQTVTN6F?usp=sharing">
-            <ListItem>
-              <ListItemIcon><FolderIcon /></ListItemIcon>
-              <ListItemText primary="Area Minutes Archive" />
-            </ListItem>
-          </a>
-          <a target="_new" style={{ textDecoration: 'none' }} href="https://drive.google.com/drive/folders/1CbOPzwhE5LVG0-ZX1NlwvoEgwCoGkfmt">
-            <ListItem>
-              <ListItemIcon><FileCopyIcon /></ListItemIcon>
-              <ListItemText primary="Literature Order Forms" />
-            </ListItem>
-          </a>
-        </List>
-      </div>
-    </div >
-  )
+        <Typography style={{ color: '#225c83' }} variant="h4">Trust Servant Resources</Typography>
+        <div>
+          <List dense={true}>
+            <a target="_new" style={{ textDecoration: 'none' }} href="https://drive.google.com/drive/folders/19EQDeMd0vpdiZDd9Bai_OOOXsQTVTN6F?usp=sharing">
+              <ListItem>
+                <ListItemIcon><FolderIcon /></ListItemIcon>
+                <ListItemText primary="Area Minutes Archive" />
+              </ListItem>
+            </a>
+            <a target="_new" style={{ textDecoration: 'none' }} href="https://drive.google.com/drive/folders/1CbOPzwhE5LVG0-ZX1NlwvoEgwCoGkfmt">
+              <ListItem>
+                <ListItemIcon><FileCopyIcon /></ListItemIcon>
+                <ListItemText primary="Literature Order Forms" />
+              </ListItem>
+            </a>
+            {this.state.links.map(({ linktext, linkurl }, ix) => {
+              return (
+                <a key={ix} target="_new" style={{ textDecoration: 'none' }} href={linkurl}>
+                  <ListItem>
+                    <ListItemIcon><LinkIcon /></ListItemIcon>
+                    <ListItemText primary={linktext} />
+                  </ListItem>
+                </a>
+              )
+            })}
+          </List>
+        </div>
+      </div >
+    )
+  }
 }
 
 export default TrustedServants
