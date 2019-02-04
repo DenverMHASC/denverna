2ff9af6a9682f2568649f88aa6de6d820ad84a97
Webservant MHASC <webservant.mhasc@gmail.com>
2019-02-04 10:04:38 -0700

    light refactorting for TS page

--- src/views/TrustedServants.js
+++ src/views/TrustedServants.js
@@ -53,7 +53,6 @@ class TrustedServants extends React.Component {
     })
 
     GetSheetDone.labeledCols(TRUSTED_SERVANT_GUIDELINES_ID).then(sheet => {
-      console.log(sheet.data)
       this.setState({ guidelines: sheet.data })
     })
   }
@@ -62,127 +61,100 @@ class TrustedServants extends React.Component {
     const { classes } = this.props
     return (
       <OuterContainer style={{ justifyContent: 'space-between' }}>
-        <Grid style={{ maxWidth: '445px', minWidth: '445px', margin: "20px" }} item md={4} sm={12} >
-          <Card>
-            <CardHeader
-              title={<Typography style={{ color: '#225c83' }} variant='h5'>Mile High Area Service Meetings</Typography>}
-              subheader={<Typography style={{ color: '#225c83' }}>Attend a subcommittee meeting and get involved!</Typography>}
-            />
-            <CardContent>
-              <iframe
-                src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;mode=AGENDA&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=9ip36bqq8qgdusokmkb96n486k%40group.calendar.google.com&amp;color=%2329527A&amp;ctz=America%2FDenver"
-                style={{ border: '0', margin: '0 auto', width: '100%', height: '400px' }} frameBorder="0" scrolling="no"></iframe>
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid style={{ maxWidth: '445px', minWidth: '445px', margin: "20px" }} item md={4} sm={12} >
-          <Card>
-            <CardHeader
-              title={<Typography style={{ color: '#225c83' }} variant='h5'>Contact Information</Typography>}
-              subheader={<Typography style={{ color: '#225c83' }}>Email addresses to contact area trusted servants.</Typography>}
-            />
-            <CardContent>
-              <div style={{ width: '100%' }}>
-                <List dense={false}>
-                  {this.state.contactInfo.map(({ emaillabel, emailaddr }, ix) => {
-                    return (
-                      <ListItem key={ix}>
-                        <ListItemIcon><EmailIcon /></ListItemIcon>
-                        <ListItemText
-                          primary={<Typography style={{ color: '#225c83', fontSize: '16px' }}>{emailaddr}</Typography>}
-                          secondary={<Typography style={{ color: '#225c83' }}>{emaillabel}</Typography>}
-                        />
-                      </ListItem>
-                    )
-                  })}
-                </List>
-              </div>
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid style={{ maxWidth: '445px', minWidth: '445px', margin: "20px" }} item md={4} sm={12} >
-          <Card>
-            <CardHeader
-              title={<Typography style={{ color: '#225c83' }} variant='h5'>Trusted Servant Resources</Typography>}
-              subheader={<Typography style={{ color: '#225c83' }}>Helpful resources from those involved in service.</Typography>}
-            />
-            <CardContent>
-              <div style={{ width: '100%' }}>
-                <List dense={false}>
-                  <a target="_new" style={{ textDecoration: 'none' }} href="https://drive.google.com/drive/folders/19EQDeMd0vpdiZDd9Bai_OOOXsQTVTN6F?usp=sharing">
-                    <ListItem>
-                      <ListItemIcon><FolderIcon /></ListItemIcon>
-                      <ListItemText
-                        primary={<Typography style={{ color: '#225c83', fontSize: '16px' }}>Area Minutes Archive</Typography>}
-                      />
-                    </ListItem>
-                  </a>
-                  {this.state.links.map(({ linktext, linkurl }, ix) => {
-                    return (
-                      <a key={ix} target="_new" style={{ textDecoration: 'none' }} href={linkurl}>
-                        <ListItem>
-                          <ListItemIcon><LinkIcon /></ListItemIcon>
-                          <ListItemText primary={<Typography style={{ color: '#225c83', fontSize: '16px' }}>{linktext}</Typography>} />
-                        </ListItem>
-                      </a>
-                    )
-                  })}
-                </List>
-              </div>
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid style={{ maxWidth: '445px', minWidth: '445px', margin: "20px" }} item md={4} sm={12} >
-          <Card>
-            <CardHeader
-              title={<Typography style={{ color: '#225c83' }} variant='h5'>Forms</Typography>}
-              subheader={<Typography style={{ color: '#225c83' }}>Useful forms for area meetings.</Typography>}
-            />
-            <CardContent>
-              <div style={{ width: '100%' }}>
-                <List dense={false}>
-                  {this.state.forms.map(({ linktext, linkurl }, ix) => {
-                    return (
-                      <a key={ix} target="_new" style={{ textDecoration: 'none' }} href={linkurl}>
-                        <ListItem>
-                          <ListItemIcon><FileCopyIcon /></ListItemIcon>
-                          <ListItemText primary={<Typography style={{ color: '#225c83', fontSize: '16px' }}>{linktext}</Typography>} />
-                        </ListItem>
-                      </a>
-                    )
-                  })}
-                </List>
-              </div>
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid style={{ maxWidth: '445px', minWidth: '445px', margin: "20px" }} item md={4} sm={12} >
-          <Card>
-            <CardHeader
-              title={<Typography style={{ color: '#225c83' }} variant='h5'>Guidelines</Typography>}
-              subheader={<Typography style={{ color: '#225c83' }}>Mile High Area subcommittee guidelines.</Typography>}
-            />
-            <CardContent>
-              <div style={{ width: '100%' }}>
-                <List dense={false}>
-                  {this.state.guidelines.map(({ label, link }, ix) => {
-                    return (
-                      <a key={ix} target="_new" style={{ textDecoration: 'none' }} href={link}>
-                        <ListItem>
-                          <ListItemIcon><FileCopyIcon /></ListItemIcon>
-                          <ListItemText primary={<Typography style={{ color: '#225c83', fontSize: '16px' }}>{label}</Typography>} />
-                        </ListItem>
-                      </a>
-                    )
-                  })}
-                </List>
-              </div>
-            </CardContent>
-          </Card>
-        </Grid>
+        <CalendarCard />
+        <GoogleSheetCard
+          title='Contact Information'
+          subtitle='Email addresses to contact area trusted servants.'
+          data={this.state.contactInfo}
+          hasSecondary={true}
+          icon={<EmailIcon />}
+        />
+        <GoogleSheetCard
+          title='Trusted Servant Resources'
+          subtitle='Helpful resources from those involved in service.'
+          data={this.state.links}
+          icon={<LinkIcon />}
+        >
+          <a target="_new" style={{ textDecoration: 'none' }} href="https://drive.google.com/drive/folders/19EQDeMd0vpdiZDd9Bai_OOOXsQTVTN6F?usp=sharing">
+            <ListItem>
+              <ListItemIcon><FolderIcon /></ListItemIcon>
+              <ListItemText
+                primary={<Typography style={{ color: '#225c83', fontSize: '16px' }}>Area Minutes Archive</Typography>}
+              />
+            </ListItem>
+          </a>
+        </GoogleSheetCard>
+        <GoogleSheetCard
+          title='Forms'
+          subtitle='Useful forms for area meetings.'
+          data={this.state.forms}
+          icon={<FileCopyIcon />}
+        />
+        <GoogleSheetCard
+          title='Guidelines'
+          subtitle='Mile High Area subcommittee guidelines.'
+          data={this.state.guidelines}
+          icon={<FileCopyIcon />}
+        />
       </OuterContainer>
     )
   }
 }
 
 export default withStyles(styles)(TrustedServants)
+
+const CalendarCard = () => (
+  <Card style={{ marginTop: '10px', marginBottom: '10px' }}>
+    <CardHeader
+      title={<Typography style={{ color: '#225c83' }} variant='h5'>Mile High Area Service Meetings</Typography>}
+      subheader={<Typography style={{ color: '#225c83' }}>Attend a subcommittee meeting and get involved!</Typography>}
+    />
+    <CardContent>
+      <iframe
+        src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;mode=AGENDA&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=9ip36bqq8qgdusokmkb96n486k%40group.calendar.google.com&amp;color=%2329527A&amp;ctz=America%2FDenver"
+        style={{ border: '0', margin: '0 auto', width: '100%', height: '400px' }} frameBorder="0" scrolling="no"></iframe>
+    </CardContent>
+  </Card>
+
+)
+
+const GoogleSheetCard = ({ title, subtitle, data, icon, children, hasSecondary }) => {
+  if (!data[0]) return null
+  const keyNames = Object.keys(data[0])
+  return (
+    <Card style={{ marginTop: '10px', marginBottom: '10px' }}>
+      <CardHeader
+        title={<Typography style={{ color: '#225c83' }} variant='h5'>{title}</Typography>}
+        subheader={<Typography style={{ color: '#225c83' }}>{subtitle}</Typography>}
+      />
+      <CardContent>
+        <div style={{ width: '100%' }}>
+          <List dense={false}>
+            {children}
+            {data.map((d, ix) => {
+              if (hasSecondary) {
+                return (
+                  <ListItem key={ix}>
+                    <ListItemIcon><EmailIcon /></ListItemIcon>
+                    <ListItemText
+                      primary={<Typography style={{ color: '#225c83', fontSize: '16px' }}>{d[keyNames[0]]}</Typography>}
+                      secondary={<Typography style={{ color: '#225c83' }}>{d[keyNames[1]]}</Typography>}
+                    />
+                  </ListItem>
+                )
+              }
+              return (
+                <a key={ix} target="_new" style={{ textDecoration: 'none' }} href={d[keyNames[1]]}>
+                  <ListItem>
+                    <ListItemIcon>{icon}</ListItemIcon>
+                    <ListItemText primary={<Typography style={{ color: '#225c83', fontSize: '16px' }}>{d[keyNames[0]]}</Typography>} />
+                  </ListItem>
+                </a>
+              )
+            })}
+          </List>
+        </div>
+      </CardContent>
+    </Card>
+  )
+}
