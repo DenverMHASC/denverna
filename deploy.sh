bebf7fd67755fd8a2fe423004f51e698c7b9b72d
Webservant MHASC <webservant.mhasc@gmail.com>
2019-02-06 16:05:11 -0700

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -22,9 +22,9 @@ if [[ "$TRAVIS_BRANCH/$TRAVIS_PULL_REQUEST" == "master/false" ]]; then
         echo "No files to update"
     else
         echo "Uploading dist/bundle.js"
-        curl --ftp-create-dirs -T dist/main.js --user $FTP_USERNAME:$FTP_PASSWORD ftp://ftp.nacolorado.org/dist/bundle.js
+        curl --ftp-create-dirs -T dist/bundle.js --user $FTP_USERNAME:$FTP_PASSWORD ftp://ftp.nacolorado.org/dist/bundle.js
         echo "Uploading dist/bundle.js.map"
-        curl --ftp-create-dirs -T dist/main.js.map --user $FTP_USERNAME:$FTP_PASSWORD ftp://ftp.nacolorado.org/dist/bundle.js.map
+        curl --ftp-create-dirs -T dist/bundle.js.map --user $FTP_USERNAME:$FTP_PASSWORD ftp://ftp.nacolorado.org/dist/bundle.js.map
     fi
 
 fi
