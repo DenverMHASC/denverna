663b7f2a5d2030cf649e62e80da4aa850c09d565
denvernaaf <webservant.MHASC@gmail.com>
2020-03-14 17:46:50 -0600

    sort alphabetically

--- src/views/Covid.js
+++ src/views/Covid.js
@@ -1,15 +1,10 @@
 import React, { Component } from 'react'
+import { sortBy } from 'lodash'
 import withWidth from '@material-ui/core/withWidth'
 import GetSheetDone from 'get-sheet-done'
 import OuterContainer from '../components/OuterContainer'
 import moment from 'moment'
 import AlertsTable from '../components/CovidAlertsTableLg'
-
-
-import { reverse } from 'lodash'
-
-
-
 const DATA = '1GB9cm8R5lMVQo2Hq1-mdudEkk2wRjG3nD2EDkTg2QAE'
 
 class Covid extends Component {
@@ -21,14 +16,13 @@ class Covid extends Component {
       alerts: []
     }
 
-
   }
 
   componentDidMount() {
     GetSheetDone.labeledCols(DATA).then(sheet => {
-      reverse(sheet.data)
+      const alerts = sortBy(sheet.data, (sheetData => sheetData.meetingname))
       this.setState({
-        alerts: sheet.data.filter(alert => {
+        alerts: alerts.filter(alert => {
           const endOfEventDate = moment(alert.expiredate, 'M-D-YYYY').endOf('day').unix() + 1 // I hate date math.
           const endOfDay = moment().endOf('day').unix()
           if (endOfEventDate <= endOfDay) {
