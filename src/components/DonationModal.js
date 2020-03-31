030e33f59bbf7c5bdbb996ede5189f246a72d598
denvernaaf <webservant.MHASC@gmail.com>
2020-03-31 17:29:50 -0600

    Add venmo modal

--- src/components/DonationModal.js
+++ src/components/DonationModal.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
@@ -7,6 +6,11 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import withMobileDialog from '@material-ui/core/withMobileDialog';
+import venmoQr from '../../assets/venmo-qr.png'
+import Typography from '@material-ui/core/Typography'
+import A from '../components/A'
+
+
 
 class ResponsiveDialog extends React.Component {
 
@@ -15,30 +19,46 @@ class ResponsiveDialog extends React.Component {
   }
 
   render() {
-    const { fullScreen } = this.props;
+    const { fullScreen, handleClose, open } = this.props;
 
     return (
       <div>
         <Dialog
           fullScreen={fullScreen}
-          open={this.props.open}
-          onClose={this.props.handleClose}
+          open={open}
+          onClose={handleClose}
           aria-labelledby="responsive-dialog-title"
         >
-          <DialogTitle id="responsive-dialog-title">{"Make a donation"}</DialogTitle>
+          <DialogTitle style={{ color: '#0f5d88' }} align="center" id="responsive-dialog-title">{"Make a Venmo Donation to the Mile High Area."}</DialogTitle>
           <DialogContent>
-            <DialogContentText>
-              Let Google help apps determine location. This means sending anonymous location data to
-              Google, even when no apps are running.
-            </DialogContentText>
+            <div>
+              <Typography variant='subtitle1' align="center" color="primary">
+                @MileHighAreaNA
+              </Typography>
+              <img
+                style={{
+                  width: '150px',
+                  height: 'auto',
+                  margin: '0 auto 10px',
+                  display: 'block',
+                  padding: '3px',
+                  border: '1px solid rgb(34, 92, 131)'
+                }}
+                src={venmoQr}
+              />
+              <Typography align="center">
+                Make a 7th tradition donation to @MileHighAreaNA on the venmo mobile app. You may also scan the QR above. Our 7th tradition states that "Every NA group ought to be fully self-supporting, declining outside contributions.
+                <br /> <A target="_blank" href="https://docs.google.com/document/d/12Y9wmMis4Rce8oGe1hCRFdxtnAU9nriIpVnXgUo1CNQ/edit?ts=5e824d55">Click here for more detailed instructions</A>
+              </Typography>
+            </div>
           </DialogContent>
           <DialogActions>
-            <Button onClick={this.props.handleClose} color="primary" autoFocus>
+            <Button onClick={handleClose} color="primary" autoFocus>
               Close
             </Button>
           </DialogActions>
         </Dialog>
-      </div>
+      </div >
     );
   }
 }
--- src/views/Home.js
+++ src/views/Home.js
@@ -7,71 +7,92 @@ import CardMedia from '@material-ui/core/CardMedia'
 import Typography from '@material-ui/core/Typography'
 
 import OuterContainer from '../components/OuterContainer'
-import A from '../components/A'
 import ContactList from '../components/ContactList'
+import DonationModal from '../components/DonationModal'
 import skyline from '../../assets/skyline.jpg'
 
-const Home = (props) => {
-  const { history, classes, width } = props
-  return (
-    <OuterContainer width={width}>
-      <Typography variant='h3' style={{ margin: '0 auto', textAlign: 'center' }} color='primary'>
-        Welcome to the Mile High Area
+class Home extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      open: false
+    }
+
+    this.handleOpen = this.handleOpen.bind(this)
+    this.handleClose = this.handleClose.bind(this)
+  }
+
+  handleOpen() {
+    this.setState({ open: true })
+  }
+
+  handleClose() {
+    this.setState({ open: false })
+  }
+
+  render() {
+    const { history, classes, width } = this.props
+    return (
+      <OuterContainer width={width}>
+        <Typography variant='h3' style={{ margin: '0 auto', textAlign: 'center' }} color='primary'>
+          Welcome to the Mile High Area
       </Typography>
-      <Typography style={{ margin: '0 auto', textAlign: 'center', width: '100%' }} color='primary' >
-        The Mile High Area of Narcotics Anonymous serves Denver, the greater Denver Metro Area, Summit County, and Steamboat Springs.
+        <Typography style={{ margin: '0 auto', textAlign: 'center', width: '100%' }} color='primary' >
+          The Mile High Area of Narcotics Anonymous serves Denver, the greater Denver Metro Area, Summit County, and Steamboat Springs.
       </Typography>
-      <Button
-        variant="text"
-        style={{ marginTop: '15px', }}
-        style={{ color: '#1c84be' }}
-      >
-        Make a Donation to the Mile High Area.
+        <Button
+          variant="outlined"
+          onClick={this.handleOpen}
+          style={{ marginTop: '15px', color: '#1c84be' }}
+        >
+          Make a Donation With Venmo
       </Button>
-      <div
-        style={{ marginTop: '20px', position: 'relative' }}
-      >
-        <CardMedia
-          component="img"
-          className={classes.skyline}
-          image={skyline}
-          title="R0uge [CC BY-SA 4.0 (https://creativecommons.org/licenses/by-sa/4.0)], from Wikimedia Commons"
-          height="400"
-        />
-        <div className={classes.viewMeetings}>
+        <div
+          style={{ marginTop: '20px', position: 'relative' }}
+        >
+          <CardMedia
+            component="img"
+            className={classes.skyline}
+            image={skyline}
+            title="R0uge [CC BY-SA 4.0 (https://creativecommons.org/licenses/by-sa/4.0)], from Wikimedia Commons"
+            height="400"
+          />
+          <div className={classes.viewMeetings}>
 
-          <Typography align='center' variant='h5' style={{ margin: '0 auto', color: '#225c83' }} >
-            Recovery happens in meetings, please join us.
+            <Typography align='center' variant='h5' style={{ margin: '0 auto', color: '#225c83' }} >
+              Recovery happens in meetings, please join us.
           </Typography>
-          <Button
-            style={{ marginTop: '10px', backgroundColor: '#1c84be', color: 'white' }}
-            variant='contained'
-            onClick={() => history.push('/meetings')}
-          >
-            View Meeting List
+            <Button
+              style={{ marginTop: '10px', backgroundColor: '#1c84be', color: 'white' }}
+              variant='contained'
+              onClick={() => history.push('/meetings')}
+            >
+              View Meeting List
             </Button>
-        </div>
-
-      </div>
-      <Grid container>
-        <Grid item md={8} sm={12} >
-          <div style={{ width: '80%' }}>
-            <Typography className={classes.lowerHeader} variant='h5'>What is Narcotics Anonymous?</Typography>
-            <Typography variant='subtitle1' style={{ marginBottom: '1em', color: '#225c83' }}>
-              Narcotics Anonymous is a nonprofit Fellowship or society of men and women for whom drugs had become a major problem. We are recovering addicts who meet regularly to help each other stay clean. This is a program of complete abstinence from all drugs. There is only one requirement for membership, the desire to stop using. We suggest that you keep an open mind and give yourself a break. Our program is a set of principles written so simply that we can follow them in our daily lives. The most important thing about them is that they work.
-              <br /><br /> <i> - What is the Narcotics Anonymous Program, Basic Text (p. 9) </i>
-            </Typography>
           </div>
-        </Grid>
-        <Grid item md={4} sm={12}>
-          <Typography className={classes.lowerHeader} variant='h5'>
-            Speak to a recovering addict
+        </div>
+        <Grid container>
+          <Grid item md={8} sm={12} >
+            <div style={{ width: '80%' }}>
+              <Typography className={classes.lowerHeader} variant='h5'>What is Narcotics Anonymous?</Typography>
+              <Typography variant='subtitle1' style={{ marginBottom: '1em', color: '#225c83' }}>
+                Narcotics Anonymous is a nonprofit Fellowship or society of men and women for whom drugs had become a major problem. We are recovering addicts who meet regularly to help each other stay clean. This is a program of complete abstinence from all drugs. There is only one requirement for membership, the desire to stop using. We suggest that you keep an open mind and give yourself a break. Our program is a set of principles written so simply that we can follow them in our daily lives. The most important thing about them is that they work.
+              <br /><br /> <i> - What is the Narcotics Anonymous Program, Basic Text (p. 9) </i>
+              </Typography>
+            </div>
+          </Grid>
+          <Grid item md={4} sm={12}>
+            <Typography className={classes.lowerHeader} variant='h5'>
+              Speak to a recovering addict
         </Typography>
-          <ContactList />
+            <ContactList />
+          </Grid>
         </Grid>
-      </Grid>
-    </OuterContainer >
-  )
+        <DonationModal handleClose={this.handleClose} open={this.state.open} />
+      </OuterContainer >
+    )
+  }
 }
 
 const styles = {
